8f13cae94b5e2611c774201d847c7d1a011e93f1
John Repo <JohnRepo@example.com>
2024-04-21 23:29:43 +0100

    Add comments to the script for better understanding

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,3 @@
-user_input = input("Get some more:")
-print("Got:", user_input)
+# This script asks the user for input and then prints out the input received.
+user_input = input("Get some more:")  # Input is stored in the variable user_input.
+print("Got:", user_input)  # The input received from the user is printed out.
